5255579081176c260652ca491233b030bb1b8aa4
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-12-13 14:48:23 -0600

    Update README.md

--- README.md
+++ README.md
@@ -1,20 +1,57 @@
-# [Joorge Search](https://jorgesarricolea.com/joorge-search)
-This is a small search engine with simulated Google-like recommendations, the intention is to demonstrate my UI skills.
+# [Joorge Search](https://joorge-search.netlify.app)
+
+This is a search engine with simulated Google-like recommendations.
+
+## Table of Contents
+
+- [Joorge Search](#joorge-search)
+  - [Table of Contents](#table-of-contents)
+  - [Description](#description)
+  - [Getting Started](#getting-started)
+    - [Installation](#installation)
+  - [Features](#features)
+  - [Contributing](#contributing)
+  - [Credits](#credits)
 
 ## Description
+
 A Front-End Developer should have the ability to design friendly and responsive interfaces. A good practice is to replicate designs from applications or websites. In this project, I have decided to clone Google Search with a personal touch. Additionally, I have added a couple of functions that will make it interactive.
 
 ## Getting Started
-To get started with this project, [click here](https://jorgesarricolea.com/joorge-search) or simply clone this repository to your local machine and open the joorge-search.html file in your preferred web browser.
+
+To get started with "Joorge-Search", follow these simple steps:
+
+### Installation
+
+1. Clone the repository to your local machine:
+
+```
+git clone https://github.com/JorgeSarricolea/Joorge-Search
+```
+
+2. Navigate to the project directory:
+
+```
+cd Joorge-Search
+```
+
+3. Open the index.html file with Live Server (In VSCode) or with your favorite browser.
 
 ## Features
+
 Some of the key features of this project include:
 
-- Full responsive design.
-- Custom-made CSS styles and jQuery JavaScript.
+- **Responsiveness:** The UI works for mobile devices and desktop.
+- **Search suggestions**: A list of personalized suggestions.
+- **Keypress:** It works by pressing the search button or the "enter" key.
+
+> [!TIP]
+> In the **main.js** file you can find the suggestions hash table called "suggestionUrls" and you can add or remove suggestions according to your needs.
 
 ## Contributing
+
 While contributions to this project are welcome. If you would like to contribute, feel free to fork this repository and submit a pull request.
 
 ## Credits
+
 This project was created by [Jorge Sarricolea](https://jorgesarricolea.com). if you have any specific code questions or would like to chat about programming, feel free to contact me on [WhatsApp](https://wa.me/529381095593).
--- js/main.js
+++ js/main.js
@@ -1,126 +1,113 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const inputBox = document.querySelector("input");
+  let linkTag = document.querySelector(".search-link");
+  let webLink;
 
-    const inputBox = $("input");
-    const icon = $(".icon");
-    let linkTag = $(".search-link");
-    let webLink;
+  // Suggestions
+  let suggestions = [
+    "Calorie Calculator",
+    "Spotify",
+    "Weather",
+    "Photos",
+    "Movies",
+    "Photolens",
+    "Rock paper scissors",
+    "Breaking News",
+    "Cooking Recipes",
+    "How to lose weight",
+    "How to learn a new language",
+    "Nearby Tourist Places",
+    "How to repair something at home",
+    "Tips for improving mental health",
+    "Job Offers",
+    "How to improve productivity",
+    "Tips for saving money",
+    "How to buy something online",
+    "Meaning of a word",
+    "How to do crafts",
+    "How to prepare for an exam",
+    "Tips for improving memory",
+    "How to start a business",
+    "Best phone apps",
+    "Tips for quitting smoking",
+  ];
 
-    /* Suggestions
-    ---------------------*/
+  // Hash table for mapping suggestions to URLs
+  const suggestionUrls = {
+    "calorie calculator": "https://kcal-calc.netlify.app/",
+    "cooking recipes": "https://recipies-net.netlify.app/",
+    spotify: "https://spotifyuicln.netlify.app",
+    weather: "https://wtrapp.netlify.app/",
+    photos: "https://photo-lens.netlify.app/",
+    photolens: "https://photo-lens.netlify.app/",
+    "rock paper scissors": "https://roc-paper-scissors.netlify.app/",
+    movies: "https://the-movies-app-mocha.vercel.app",
+  };
 
-    let suggestions = [
-        "Calorie Calculator",
-        "Spotify",
-        "Weather",
-        "Breaking News",
-        "Cooking Recipes",
-        "How to lose weight",
-        "How to learn a new language",
-        "Nearby Tourist Places",
-        "How to repair something at home",
-        "Tips for improving mental health",
-        "Job Offers",
-        "How to improve productivity",
-        "Tips for saving money",
-        "How to buy something online",
-        "Meaning of a word",
-        "Movie schedules",
-        "How to do crafts",
-        "How to prepare for an exam",
-        "Tips for improving memory",
-        "How to start a business",
-        "Best phone apps",
-        "Tips for quitting smoking"
-    ];
+  // User Search Functions
+  inputBox.addEventListener("keyup", function (e) {
+    let userData = e.target.value;
+    let emptyArray = [];
+    if (userData) {
+      emptyArray = suggestions.filter(function (data) {
+        return data.toLowerCase().startsWith(userData.toLowerCase());
+      });
+      emptyArray = emptyArray.map((data) => {
+        return `<li>${data}</li>`;
+      });
+      document.querySelector(".autocom-box").classList.add("active");
+    } else {
+      document.querySelector(".autocom-box").classList.remove("active");
+    }
+    showSuggestions(emptyArray);
+  });
 
-    /* User Search Functions
-    ---------------------*/
+  // Handle "Enter" key press
+  inputBox.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      handleSearch(inputBox.value.toLowerCase()); // Convert to lowecase
+    }
+  });
 
-    inputBox.on("keyup", function (e) {
-        let userData = e.target.value;
-        let emptyArray = [];
-        if (userData) {
-            emptyArray = suggestions.filter(function (data) {
-                // The suggestions will appear according to the first letter that the user enters
-                return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-            });
-            emptyArray = emptyArray.map((data) => {
-                // Passing return data inside li tag
-                return data = `<li>${data}</li>`;
-            });
-            $(".autocom-box").addClass("active");
-        } else {
-            $(".autocom-box").removeClass("active");
-        }
-        showSuggestions(emptyArray);
+  // User Search Function Icon
+  function select(element) {
+    let selectData = element.textContent.toLowerCase(); // Convert to lowecase
+    inputBox.value = selectData;
+    handleSearch(selectData);
+  }
+
+  // Handle search action
+  function handleSearch(query) {
+    // Check if the suggestion exists in the hash table
+    if (suggestionUrls.hasOwnProperty(query)) {
+      webLink = suggestionUrls[query];
+      linkTag.setAttribute("href", webLink);
+      linkTag.click();
+    } else {
+      webLink = `https://www.google.com/search?q=${query}`;
+      linkTag.setAttribute("href", webLink);
+      linkTag.click();
+    }
+    // Reload the main page to new search input
+    location.reload();
+  }
+
+  // List Data Function
+  function showSuggestions(list) {
+    let listData;
+    if (!list.length) {
+      userValue = inputBox.value;
+      listData = `<li>${userValue}</li>`;
+    } else {
+      listData = list.join("");
+    }
+    document.querySelector(".autocom-box").innerHTML = listData;
+
+    // Onclick function in listData
+    document.querySelectorAll(".autocom-box li").forEach(function (item) {
+      item.addEventListener("click", function () {
+        select(this);
+      });
     });
-
-    /* User Search Function Icon
-    ---------------------*/
-
-    function select(element) {
-        let selectData = $(element).text();
-        inputBox.val(selectData);
-        console.log(selectData);
-        icon.click(function () {
-
-            /* Switch On My Projects
-            ---------------------*/
-
-            switch (selectData) {
-                case "Calorie Calculator":
-                    webLink = `https://jorgesarricolea.com/calorie-calculator`;
-                    linkTag.attr("href", webLink);
-                    linkTag.get(0).click();
-                    break;
-
-                case "Cooking Recipes":
-                    webLink = `https://jorgesarricolea.com/recipies-net`;
-                    linkTag.attr("href", webLink);
-                    linkTag.get(0).click();
-                    break;
-
-                case "Spotify":
-                    webLink = `https://jorgesarricolea.com/spotify-ui-clon`;
-                    linkTag.attr("href", webLink);
-                    linkTag.get(0).click();
-                    break;
-
-                case "Weather":
-                    webLink = `https://jorgesarricolea.com/weather-app`;
-                    linkTag.attr("href", webLink);
-                    linkTag.get(0).click();
-                    break;
-
-                default:
-                    webLink = `https://www.google.com/search?q=${selectData}`;
-                    linkTag.attr("href", webLink);
-                    linkTag.get(0).click();
-            }
-            // Reload the main page to new search input
-            location.reload();
-        });
-        $(".autocom-box").removeClass("active");
-    }
-
-    /* List Data Function
-    ---------------------*/
-
-    function showSuggestions(list) {
-        let listData;
-        if (!list.length) {
-            userValue = inputBox.val();
-            listData = `<li>${userValue}</li>`;
-        } else {
-            listData = list.join('');
-        }
-        $(".autocom-box").html(listData);
-
-        // Onclick function in listData
-        $(".autocom-box li").each(function () {
-            $(this).click(function () {
-                select(this);
-            });
-        });
-    }
+  }
 });
